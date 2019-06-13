e67673ef4b5489b7f9670510f1c36b05d4999bb7
Greyfox <boomman@hotmail.fr>
2019-06-13 22:36:40 +0100

    file copy

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {getSticky()};
+
+let topNavBar = document.getElementById("topNavBar");
+
+let sticky = topNavBar.offsetTop;
+
+function getSticky() {
+    if(window.pageYOffset >= sticky) {
+        topNavBar.classList.add("sticky");
+    } else {
+        topNavBar.classList.remove("sticky");
+    }
+}
